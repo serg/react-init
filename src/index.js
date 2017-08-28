bc548d308824c860e6f0d2e61fdbcecdc5c23174
sfexample <sfexample@users.noreply.github.com>
2017-08-28 20:05:23 +0300

    init the React starter kit project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './css/main.scss';
+import { App } from './components/App';
+
+render(<App/>, window.document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+	filename: 'main.css'
+});
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['react', 'es2015']
+					}
+				}
+			},
+			{
+				test: /\.(scss|sass)$/,
+				use: extractPlugin.extract({
+					use: ['css-loader', 'sass-loader']
+				})
+			},
+			{
+				test: /\.html$/,
+				use: ['html-loader']
+			},
+			{
+				test: /\.(jpg|png)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'img/'
+					}
+				}]
+			}
+		]
+	},
+	plugins: [
+		extractPlugin,
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new CleanWebpackPlugin(['dist'])
+	]
+};
