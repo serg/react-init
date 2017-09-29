d83b38015e8162c297fb7e60324eef4355424ba8
sfexample <sfexample@users.noreply.github.com>
2017-09-29 20:40:45 +0300

    respect spaces, avoid tabs

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import reactLogo from '../img/react-logo.png';
 
 export class App extends React.Component {
-	render() {
-		return (
-			<div className="main-container">
-	      		<h1>Hello, React!</h1>
-	      		<img src={reactLogo} width="300" height="300" alt="React logo"/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="main-container">
+        <h1>Hello, React!</h1>
+        <img src={reactLogo} width="300" height="300" alt="React logo"/>
+      </div>
+    );
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -4,54 +4,54 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
-	filename: 'main.css'
+  filename: 'main.css'
 });
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['react', 'es2015']
-					}
-				}
-			},
-			{
-				test: /\.(scss|sass)$/,
-				use: extractPlugin.extract({
-					use: ['css-loader', 'sass-loader']
-				})
-			},
-			{
-				test: /\.html$/,
-				use: ['html-loader']
-			},
-			{
-				test: /\.(jpg|png)$/,
-				use: [{
-					loader: 'file-loader',
-					options: {
-						name: '[name].[ext]',
-						outputPath: 'img/'
-					}
-				}]
-			}
-		]
-	},
-	plugins: [
-		extractPlugin,
-		new HtmlWebpackPlugin({
-			template: './src/index.html'
-		}),
-		new CleanWebpackPlugin(['dist'])
-	]
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ]
 };
