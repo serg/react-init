e1c56dda37c4fc2a4fbc35efeea733031547bb85
serg <contact@sergfurtak.com>
2019-01-07 16:56:22 +0200

    update the React and Babel dependencies. improve the Webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -3,52 +3,46 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: 'none',
+var config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              'react',
-              ['env', {
-                'targets': {
-                  'browsers': ['last 2 versions', '> 1%']
-                },
-                'modules': false
-              }]
-            ]
-          }
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-react',
+            ['@babel/preset-env', {
+              'targets': {
+                'browsers': ['last 2 versions', '> 1%']
+              },
+              'modules': false
+            }]
+          ]
         }
-      },
-      {
-        test: /\.(scss|sass)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.html$/,
-        use: ['html-loader']
-      },
-      {
-        test: /\.(jpg|png)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'img/'
-          }
-        }]
       }
-    ]
+    }, {
+      test: /\.(scss|sass)$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+    }, {
+      test: /\.html$/,
+      use: ['html-loader']
+    }, {
+      test: /\.(jpg|png|svg|gif|webp|jpeg|ico)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'img/'
+        }
+      }]
+    }]
   },
   plugins: [
     new MiniCssExtractPlugin({
@@ -56,7 +50,16 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    }),
-    new CleanWebpackPlugin(['dist'])
+    })
   ]
+};
+
+module.exports = (env, argv) => {
+  var isProduction = argv.mode === 'production';
+
+  if (isProduction) {
+    config.plugins.push(new CleanWebpackPlugin(['dist']));
+  }
+
+  return config;
 };
