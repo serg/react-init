13f3e1cb009c11821cfeb4b443d7076f1c95b63c
serg <contact@sergfurtak.com>
2020-01-25 10:16:06 +0200

    update for using functional components

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import reactLogo from '../img/react-logo.png';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <div className="main-container">
-        <h1>Hello, React!</h1>
-        <img src={reactLogo} width="300" height="300" alt="React logo"/>
-      </div>
-    );
-  }
+export default () => {
+  return (
+    <div className="main-container">
+      <h1>Hello, React!</h1>
+      <img src={reactLogo} width="300" height="300" alt="React logo"/>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import './css/main.scss';
-import { App } from './components/App';
+import App from './components/App';
 
 render(<App/>, window.document.getElementById('app'));
